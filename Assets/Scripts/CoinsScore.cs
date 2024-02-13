012fc4acef88bb674a9e7af30627fd7e0a4c79b2
Andrey Isachov <roki.forever4@gmail.com>
2024-02-13 13:23:25 +0300

    текст

--- Assets/Scripts/CoinsScore.cs
+++ Assets/Scripts/CoinsScore.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinsScore : MonoBehaviour
 {
 
-    [SerializeField] private int coins = 0;
-    //[SerializeField] private Text
+    [SerializeField] private int _coins = 100;
+    [SerializeField] private TextMeshProUGUI _text;
 
-    //public void SetScore()
-    //{
+    private void Start()
+    {
+        SetScore();
+    }
+    public void SetScore()
+    {
+        _text.text = $"Ñoins: {_coins}";
+    }
 
-    //}
+    public void BuyItem(int cost)
+    {
+        if (_coins > cost)
+            _coins -= cost;
+    }
 }
